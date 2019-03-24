05dd64bd8c055737dce5b804660064c1e3155313
Stefon Martin <martin.stefon@gmail.com>
2019-03-24 11:57:03 -0700

    fixed keys file

--- app.js
+++ app.js
@@ -29,6 +29,26 @@ app.get('/', (req,res) =>{
     res.render('index');
 })
 
+app.get('/success', (req,res) =>{
+    res.render('success');
+})
+
+//Charge Route
+app.post('/charge', (req,res)=>{
+    const amount = 2500;
+   stripe.customers.create({
+       email: req.body.stripeEmail,
+        source: req.body.stripeToken
+   })
+   .then(customer => stripe.charges.create({
+       amount,
+       description: 'Web Dev Ebook',
+       currency: 'usd',
+       customer: customer.id
+   }))
+   .then(charge => res.render('success'))
+
+});
 const port = process.env.PORT || 5000;
 
 app.listen(port, () =>{
---
+++ config/keys_dev.js
@@ -0,0 +1,4 @@
+module.exports = {
+    stripePublishableKey: 'pk_test_AydB8afsjFVRSWs6b17a3k8900kPNZYbLJ',
+    stripeSecretKey: 'sk_test_oIyaMinMOmn0cOtmF1DZ2LYD00DrVckf8F'
+}
